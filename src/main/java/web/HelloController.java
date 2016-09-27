979d02fdaafd3495071269a6ff3b7088327c5654
liujia <liujia@xingxingcar.com>
2016-09-27 10:11:21 +0800

    初建一个测试

---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+ @RequestMapping("/hello")
+ public ModelAndView hello(@RequestParam(value="name", defaultValue="world") String name){
+  System.out.println("hello " + name);
+  Map<String, Object> map = new HashMap<String,Object>();
+  map.put("name", name);
+  return new ModelAndView("/hello",map);
+ }
+}
